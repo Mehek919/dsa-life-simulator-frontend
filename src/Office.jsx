c35cb3eee380fe8bebd9e120c8261639f7c73c21
Mehek919 <sapnakoolwal2021@gmail.com>
2026-06-11 14:52:52 +0530

    done changes in office.jsx

--- src/Office.jsx
+++ src/Office.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import API_BASE from './config';
